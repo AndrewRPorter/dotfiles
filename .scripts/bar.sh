5afadcd6618d6dfbae29f5a79010ff1057299fc5
AndrewRPorter <pavelow53.andrew@gmail.com>
2019-06-02 11:28:52 -0400

    Andrew: added scripts for starting polybar on startup and fixed kitty configuration location

---
+++ .scripts/bar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/polybar -r -q example &
--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Link scripts directory
+rm -r ~/.scripts; ln -s ~/dotfiles/.scripts ~/.scripts
+
+# Link configuration files
 rm ~/.vimrc; ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 rm ~/.zshrc; ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
-rm ~/kitty.conf; ln -s ~/dotfiles/kitty/kitty.conf ~/kitty.conf
+rm ~/.config/kitty/kitty.conf; ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+rm ~/.config/polybar/config; ln -s ~/dotfiles/polybar/config ~/.config/polybar/config
