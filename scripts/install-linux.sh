6470f8dca34e818cb818b07a53c29ac5db833086
Andrew Porter <porter.r.andrew@gmail.com>
2026-04-10 20:04:23 +0000

    Combine apt installs

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -4,11 +4,8 @@ sudo apt-get update
 
 # note we pipe "yes n" into this apt install to preserve
 # the z login file. Otherwise, this install will overwrite it
-echo "[dotfiles] installing ZSH"
-yes n | sudo apt -y install zsh
-
-echo "[dotfiles] installing VIM"
-sudo apt -y install vim
+echo "[dotfiles] installing ZSH and VIM"
+yes n | sudo apt -y install zsh vim
 
 # set ZSH as default shell
 if [[ "$SHELL" != "/bin/zsh"* ]]; then
