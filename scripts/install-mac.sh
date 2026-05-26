7444764e96517682a1bd6a17902580a813114678
AndrewRPorter <porter.r.andrew@gmail.com>
2026-05-25 21:14:23 -0400

    Add tmux installation and config to mac setup

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -3,4 +3,8 @@
 echo "[dotfiles] installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install stats
+brew install stats tmux
+
+# symlink tmux config
+cwd=$(cd "$(dirname "$0")/.." && pwd)
+ln -sf $cwd/.tmux.conf ~/.tmux.conf
