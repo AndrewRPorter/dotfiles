2b06c102b853d8723165c758f8a19240b820d820
AndrewRPorter <porter.r.andrew@gmail.com>
2026-09-01 20:10:57 -0400

    Install tmux package manager

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -7,6 +7,13 @@ fi
 
 brew install stats tmux
 
+# install tmux plugin manager
+if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
+    echo "[dotfiles] installing TPM"
+    mkdir -p "$HOME/.tmux/plugins"
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
+
 # symlink tmux config
 cwd=$(cd "$(dirname "$0")/.." && pwd)
 ln -sf $cwd/.tmux.conf ~/.tmux.conf
